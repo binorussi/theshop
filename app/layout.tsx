6b9bf73741fef2a34695ba0556e7f169db7b0a22
binorussi <70542605+binorussi@users.noreply.github.com>
2026-08-19 20:38:26 +0000

    Fix production prerendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Suspense } from 'react';
 import './globals.css';
 import { CartProvider } from '@/app/context/CartContext';
 import Header from '@/components/layout/Header';
@@ -22,8 +23,12 @@ export default function RootLayout({
       <body className={inter.className}>
         <CartProvider>
           <div className="flex min-h-screen flex-col bg-gray-50 text-gray-900">
-            <Header />
+            <Suspense fallback={null}>
+              <Header />
+            </Suspense>
+
             <div className="flex-1">{children}</div>
+
             <Footer />
           </div>
         </CartProvider>
