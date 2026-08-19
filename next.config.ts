d082c2fe6fd3fa6040a9dfee47617f117744e03e
binorussi <70542605+binorussi@users.noreply.github.com>
2026-08-19 20:46:44 +0000

    Fix Vercel deployment configuration

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Configures Next.js to build a standalone Node.js server bundle for Plesk
-  output: 'standalone',
-
   allowedDevOrigins: [
-    '172.26.48.1', 
-    'localhost:3000',
-    '*.ngrok-free.dev'
+    "172.26.48.1",
+    "localhost:3000",
+    "*.ngrok-free.dev",
   ],
 
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
   },
